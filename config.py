b72add26781312d809c5fbe8990e8620b1e004ee
ReWoxi <64690769+Meinos10@users.noreply.github.com>
2022-09-18 14:05:57 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+SPOTIPY_CLIENT_ID = # spotify developer portal client_id
+SPOTIPY_CLIENT_SECRET = # spotify developer portal client_secret_id
+SPOTIPY_REDIRECT_URI = 'http://localhost:8888/callback'
+SCOPE = "user-read-currently-playing"
+MARKET = "TR"
+api_id = # my.telegram.org app_id
+api_hash = # my.telegram.org api_hash
+token = # t.me/BotFather new bot token
